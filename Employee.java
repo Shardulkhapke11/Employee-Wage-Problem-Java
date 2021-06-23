b2fe9acb3b5b82342385de69490089567c2bce09
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-23 09:01:15 +0530

    Added: UC2_CalculateDaily

--- Employee.java
+++ Employee.java
@@ -1,12 +1,18 @@
-public class PresentAbsent {
+public class CalculateDaily {
 
         public static void main(String[] args){
 
             int isFullTime=1;
+            int WagePerHr=20;
+            int FullDayHr=8;
+
             double empCheck = Math.floor(Math.random()*10)%2;
             if(empCheck == isFullTime)
             {
                 System.out.println("Employee is Present");
+                int DailyEmpWage=WagePerHr*FullDayHr;
+                System.out.println("DailyEmpWage::"+DailyEmpWage);
+
             }
             else
             {
