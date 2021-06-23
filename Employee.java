2b2f27d4d1a3bc8b2459c03fa64ba0273d2dd192
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-23 08:45:58 +0530

    Added: UC1_Present_Absent

--- Employee.java
+++ Employee.java
@@ -1,5 +1,18 @@
-public class Employee {
-public static void main(String[] args){
-System.out.println("Welcome to Employee Problem");
-}
-}
+public class PresentAbsent {
+
+        public static void main(String[] args){
+
+            int isFullTime=1;
+            double empCheck = Math.floor(Math.random()*10)%2;
+            if(empCheck == isFullTime)
+            {
+                System.out.println("Employee is Present");
+            }
+            else
+            {
+                System.out.println("Employee is Abscent");
+            }
+
+        }
+
+    }
