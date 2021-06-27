ecf0032dbad89e346f11cfef60a267ea3b3aa1e7
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-27 14:24:08 +0530

    Added: UC7_Refactor_Code

---
+++ UC7_Employee_Variable.java
@@ -0,0 +1,52 @@
+package employeeweage;
+
+public class UC7_Employee_Variable {
+	public void attendance() {
+        double empCheck = Utility.getRandomInt();
+        System.out.println(empCheck);
+
+        int FullDayHr = 8;
+        int PartTimeHr = 4;
+        int WagePerHr = 20;
+        int day = 0;
+        int empHr = 0;
+        int totalWage = 0;
+       
+        while (day <= 19) {
+
+            int n = Utility.getRandomInt();
+            switch (n) {
+                case 1:
+                    System.out.println("present fullday");
+                    totalWage = (totalWage + (WagePerHr * FullDayHr));
+
+                    day++;
+                    break;
+                case 2:
+                    System.out.println("present PartTime");
+                    totalWage = (totalWage + (WagePerHr * PartTimeHr));
+
+
+                    day++;
+                    break;
+                default:
+                    System.out.println("Employee is absent. ");
+                    totalWage = (totalWage + (WagePerHr * empHr));
+                    day++;
+
+                    if( empHr == 100)
+                        break;
+            }
+        }
+        System.out.println("total wage of a month is::" + totalWage);
+
+    }
+    public static void main(String[] args) {
+
+            System.out.println("Welcome to Employee Wage Computation");
+           
+            UC7_Employee_Variable emp = new UC7_Employee_Variable();
+			emp.attendance();
+        }
+
+}
